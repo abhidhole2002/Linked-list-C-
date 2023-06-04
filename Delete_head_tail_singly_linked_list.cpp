f78f2a6b04789dd14708ea821d08d779f194bf75
abhidhole2002 <134376307+abhidhole2002@users.noreply.github.com>
2023-06-05 00:03:36 +0530

    Add files via upload

---
+++ Delete_head_tail_singly_linked_list.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class slist {
+    int data;
+    slist* next;
+    slist* head;
+
+public:
+    
+
+    slist() {
+        head = NULL;
+    }
+
+    void addhead(int x) {
+        slist* newnode = new slist();
+        newnode->data = x;
+        newnode->next = head;
+        head = newnode;
+    }
+
+    void print() {
+        slist* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+};
+
+int main() {
+    
+    int size;
+    cout<<"enter size of array : ";
+    cin>>size;
+
+    int a[size];
+    cout<<"enter elements : ";
+    for(int i=0;i<size;i++)
+    {
+        cin>>a[i];
+    }
+    slist obj;
+    for(int i=0;i<size;i++)
+    {
+        obj.addhead(a[i]);
+    }
+
+    
+    obj.print();
+}
