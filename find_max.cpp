31963354c6a982f5f348397b540b3a232a9758ee
abhidhole2002 <134376307+abhidhole2002@users.noreply.github.com>
2023-06-09 00:53:47 +0530

    Add files via upload

---
+++ find_max.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+class slist
+{
+    int data;
+    slist *head,*next;
+
+    public:
+    slist()
+    {
+        head=NULL;
+
+    }
+
+    void addhead(int x)
+    {
+        slist* new_node = new slist();
+        new_node->data = x;
+
+        new_node->next = head;
+        head = new_node;
+    }
+
+    void getMax()
+    {
+        int max = head->data;
+
+        slist* temp= head;
+
+        while (temp != NULL)
+        {
+            if (temp->data < max)
+            {
+                max = temp->data;
+            }
+
+            temp = temp->next;
+        }
+
+        cout<<"the maximum in this list ==>> " <<max;
+    }
+
+    void print()
+    {
+        for(slist* temp=head;temp!=NULL;temp=temp->next)
+        {
+            cout<<temp->data<<" ";
+        }
+    }
+};
+
+ 
+int main()
+{
+    slist obj;
+    obj.addhead(10);
+    obj.addhead(11);
+    obj.addhead(12);
+    obj.print();
+
+    cout<<endl;
+
+    obj.getMax();
+
+
+ 
+ 
+}
